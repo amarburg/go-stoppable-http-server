543a4ed460a3b13a56821c81ddcc95e30341ad50
Aaron Marburg <amarburg@apl.washington.edu>
2017-02-07 16:41:51 -0800

    Changed API slightly.

--- stoppable_http_server.go
+++ stoppable_http_server.go
@@ -5,72 +5,76 @@ import "net/url"
 import "fmt"
 import "sync"
 import "net"
+
 //import "log"
 
 import "github.com/hydrogen18/stoppableListener"
 
 type SLServer struct {
-  wg  sync.WaitGroup
-  sl  *stoppableListener.StoppableListener
-  server *http.Server
-  Url string
+	wg     sync.WaitGroup
+	sl     *stoppableListener.StoppableListener
+	server *http.Server
+	Url    string
 }
 
 type HttpConfig struct {
-  host  string
-  port  int
+	Host string
+	Port int
 }
 
-func HttpServer( configFuncs ...func( *HttpConfig ) )  (*SLServer) {
+func StartServer(configFuncs ...func(*HttpConfig)) *SLServer {
 
-  config := HttpConfig{
-    host: "127.0.0.1",
-    port: 4567,
-  }
+	// Default configuration
+	config := HttpConfig{
+		Host: "127.0.0.1",
+		Port: 4567,
+	}
 
-  for _,f := range configFuncs { f( &config ) }
+	for _, f := range configFuncs {
+		f(&config)
+	}
 
-    srvIp := fmt.Sprintf("%s:%d", config.host, config.port )
-    originalListener, err := net.Listen("tcp", srvIp)
-    if err != nil {
-      panic(err)
-    }
+	srvIp := fmt.Sprintf("%s:%d", config.Host, config.Port)
 
-    //fmt.Printf("Starting web server at %s\n", url)
+	originalListener, err := net.Listen("tcp", srvIp)
+	if err != nil {
+		panic(err)
+	}
 
-    sl, err := stoppableListener.New(originalListener)
-    if err != nil {
-      panic(err)
-    }
+	//fmt.Printf("Starting web server at %s\n", url)
 
-    //var wg sync.WaitGroup
-    srv := SLServer{ server: &http.Server{},
-                    sl: sl,
-                    wg: sync.WaitGroup{},
-                    Url: fmt.Sprintf("http://%s/", srvIp ) }
+	sl, err := stoppableListener.New(originalListener)
+	if err != nil {
+		panic(err)
+	}
 
-    srv.wg.Add(1)
-    go func() {
-      defer srv.wg.Done()
-      srv.server.Serve(sl)
-    }()
+	//var wg sync.WaitGroup
+	srv := SLServer{server: &http.Server{},
+		sl:  sl,
+		wg:  sync.WaitGroup{},
+		Url: fmt.Sprintf("http://%s/", srvIp)}
 
-    return &srv
+	srv.wg.Add(1)
+	go func() {
+		defer srv.wg.Done()
+		srv.server.Serve(sl)
+	}()
+
+	return &srv
 }
-
 
 func (srv *SLServer) Stop() {
-  //fmt.Printf("Stopping web server...")
-  srv.sl.Stop()
-  srv.wg.Wait()
-  //fmt.Printf("done\n")
+	//fmt.Printf("Stopping web server...")
+	srv.sl.Stop()
+	srv.wg.Wait()
+	//fmt.Printf("done\n")
 }
 
 func (srv *SLServer) Wait() {
-  srv.wg.Wait()
+	srv.wg.Wait()
 }
 
-func (srv *SLServer) URL() (url.URL) {
-  uri,_ := url.Parse( srv.Url )
-  return *uri
+func (srv *SLServer) URL() url.URL {
+	uri, _ := url.Parse(srv.Url)
+	return *uri
 }
