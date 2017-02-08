3199595ce76fb95683483f1e1c56fbb6888d710b
Aaron Marburg <amarburg@apl.washington.edu>
2017-02-07 16:18:37 -0800

    Added test with changed port.

--- server_test.go
+++ server_test.go
@@ -1,30 +1,57 @@
 package stoppable_http_server
 
 import (
-  "testing"
-  "net/http"
-  "io"
-  "bytes"
-  "strings"
+	"bytes"
+	"io"
+	"net/http"
+	"strings"
+	"testing"
 )
 
-func BoringHandler( w http.ResponseWriter, r *http.Request) {
-  io.WriteString( w, "Response" )
+func BoringHandler(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Response")
 }
 
-func TestSingleThread( t *testing.T ) {
-  http.HandleFunc("/", BoringHandler )
-  srv := HttpServer()
-  defer srv.Stop()
+var once = false
 
-  req,err := http.Get("http://localhost:4567/")
-  if err != nil {
-    t.Error("Got error from server: %v\n", err )
-  }
-  buf := new(bytes.Buffer)
-  buf.ReadFrom( req.Body )
-  if strings.Compare( buf.String(), "Response") != 0 || err != nil {
-    t.Error("Didn't get the expected response, got \"%s\"", buf )
-  }
+func registerHandler() {
+	if once == false {
+		http.HandleFunc("/", BoringHandler)
+		once = true
+	}
+}
 
+func TestSingleThread(t *testing.T) {
+	registerHandler()
+
+	srv := StartServer()
+	defer srv.Stop()
+
+	req, err := http.Get("http://localhost:4567/")
+	if err != nil {
+		t.Error("Got error from server: %v\n", err)
+	}
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(req.Body)
+	if strings.Compare(buf.String(), "Response") != 0 || err != nil {
+		t.Error("Didn't get the expected response, got \"%s\"", buf)
+	}
+}
+
+func TestChangePort(t *testing.T) {
+	registerHandler()
+	srv := StartServer(func(config *HttpConfig) {
+		config.Port = 7654
+	})
+	defer srv.Stop()
+
+	req, err := http.Get("http://localhost:7654/")
+	if err != nil {
+		t.Error("Got error from server: %v\n", err)
+	}
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(req.Body)
+	if strings.Compare(buf.String(), "Response") != 0 || err != nil {
+		t.Error("Didn't get the expected response, got \"%s\"", buf)
+	}
 }
